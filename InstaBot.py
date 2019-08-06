543ea2db8ef573fec1d402980cf8011c5095fa09
Austin <47762009+acrumx@users.noreply.github.com>
2019-08-06 01:01:28 -0700

    Add files via upload

--- InstaBot.py
+++ InstaBot.py
@@ -1,3 +1,5 @@
+# this file is the code for automating liking mutuals instagram posts
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -46,29 +48,29 @@ class InstagramBot:
         driver = self.driver
         driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
         time.sleep(3)
-#         # Bot simulates scrolling through the webpage
-#         for i in range (1, 3):
-#             driver.execute_script("window.scrollTo(0, document.body.scrollHeight")
-#             time.sleep(3)
+        # Bot simulates scrolling through the webpage
+        for i in range (1, 3):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight")
+            time.sleep(3)
+
+        # # Searching for picture link
+        #
+        # hrefs = driver.find_element_by_tag_name('a')
+        # pic_hrefs = [elem.get_attribute('href') for elem in hrefs]
+        # pic_hrefs = [href for href in pic_hrefs if hashtag in href]
+        # print(hashtag + ' photos: ' + str(len(pic_hrefs)))
+        #
+        # for pic_href in pic_hrefs:
+        #     driver.get(pic_href)
+        #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        #     try:
+        #         driver.find_element_by_link_text("Like").click()
+        #         time.sleep(15)
+        #     except Exception as e:
+        #         time.sleep(4)
 
 
-#         # Searching for picture link
 
-#         hrefs = driver.find_element_by_tag_name('a')
-#         pic_hrefs = [elem.get_attribute('href') for elem in hrefs]
-#         pic_hrefs = [href for href in pic_hrefs if hashtag in href]
-#         print(hashtag + ' photos: ' + str(len(pic_hrefs)))
-
-#         for pic_href in pic_hrefs:
-#             driver.get(pic_href)
-#             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#             try:
-#                 driver.find_element_by_link_text("Like").click()
-#                 time.sleep(20)
-#             except Exception as e:
-#                 time.sleep(2)
-
-
-myIG = InstagramBot("instagram_username", "instagram_password")
-myIG.login()
-myIG.like_photo('fitness')
+IG = InstagramBot("username", "password")
+IG.login()
+IG.like_photo('newyork')
